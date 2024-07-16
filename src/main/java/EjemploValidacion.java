baaee3319fa5cc60138d3043735bfd9c232dc025
hellrider1990 <juanjoseguiradoruiz90@gmail.com>
2024-07-16 11:33:31 +0200

    Probando validaciones

--- src/main/java/EjemploValidacion.java
+++ src/main/java/EjemploValidacion.java
@@ -19,5 +19,34 @@ public class EjemploValidacion {
         validacion = nombre == null;
 
         System.out.println("\nY ahora es nulo? ".concat(String.valueOf(validacion)));
+
+        nombre = "";
+
+        if (nombre.isEmpty()) {
+
+            nombre = "Juan José";
+        }
+
+        System.out.println("\n".concat(nombre));
+
+        nombre = null;
+
+        if (nombre == null) {
+
+            System.out.println("\nEs nulo");
+        }
+
+        nombre = "hellrider";
+
+        int caracteres = nombre.length();
+
+        System.out.println("\nNúmero de caracteres del nombre: ".concat(String.valueOf(caracteres)));
+
+        if (nombre.length() > 8) {
+
+            nombre = "hellride";
+        }
+
+        System.out.println(nombre);
     }
 }
